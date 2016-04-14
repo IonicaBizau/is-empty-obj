e72868b63963b29a3da7171dc1c8b5cb4207d8e5
Ionică Bizău <bizauionica@gmail.com>
2016-04-14 13:19:29 +0300

    Added functionality and docs

--- example/index.js
+++ example/index.js
@@ -2,4 +2,14 @@
 
 const isEmptyObj = require("../lib");
 
-console.log(isEmptyObj());
+console.log(isEmptyObj({}));
+// true
+
+console.log(isEmptyObj([]));
+// true
+
+console.log(isEmptyObj([42]));
+// false
+
+console.log(isEmptyObj({ location: "mars" }));
+// false
